f56c978538ba42d0b35455f9cd38111d20b03c27
GAredtofu <125192790+GAredtofu@users.noreply.github.com>
2023-03-18 16:21:08 +0700

    Create databuku.h

---
+++ databuku.h
@@ -0,0 +1,104 @@
+#ifndef databuku_h //digunakan untuk
+#define databuku_h //digunakan untuk 
+
+
+// berisi data buku
+// DATA BUKU kelas 10
+std::string BindoA[4] = {
+    "BahasaIndonesia ",
+    "Kelas        : 10 ",
+    "Penulis      : Pak Jaya ",
+    "Tahun terbit : 2020 ",
+};
+
+std::string PKNA[4] = {
+    "PPKN ",
+    "Kelas        : 10 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+std::string BinggrisA[4]{
+    "BahasaInggris ",
+    "Kelas        : 10 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+// DATA BUKU KELAS 11
+std::string BindoB[4] = {
+    "BahasaIndonesia ",
+    "Kelas        : 11 ",
+    "Penulis      : Pak Jaya ",
+    "Tahun terbit : 2020 "};
+
+std::string PKNB[4] = {
+    "PPKN ",
+    "Kelas        : 11 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+std::string BinggrisB[4]{
+    "BahasaInggris ",
+    "Kelas        : 11 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+// DATA BUKU KELAS 12
+std::string BindoC[4] = {
+    "BahasaIndonesia ",
+    "Kelas        : 12 ",
+    "Penulis      : Pak Jaya ",
+    "Tahun terbit : 2020 "};
+
+std::string PKNC[4] = {
+    "PPKN ",
+    "Kelas        : 12 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+std::string BinggrisC[4] = {
+    " Bahasa Inggris ",
+    "Kelas        : 12 ",
+    "Penulis      : PAK AGUNG",
+    "tahun terbit : 2021 ",
+};
+
+std::string terjadinya_ngawi[3] = {
+    "Terjadinnya_ngawi",
+    "pengarang   : Rusdi ",
+    "tahun terbit: 2022",
+};
+
+std::string sang_rogersumatra[3] = {
+    "Sang Roger sumatra",
+    "pengarang   : gemilang ",
+    "tahun terbit: 2021",
+};
+
+std::string dilan_1990[3] = {
+    " dilan 1989"
+    "pengarang   : garena ",
+    "tahun terbit: 2019",
+};
+
+std::string bukupython[3] = {
+    "Buku_Python_advance ",
+    "pengarang   : Dea Afrizal ",
+    "tahun terbit: 2018",
+};
+std::string bukucpp[3] = {
+    "Buku C++ dasar ",
+    "pengarang   : kelas terbuka ",
+    "tahun terbit: 2018",
+};
+std::string bukujava[3] = {
+    "Buku Java dasar",
+    "pengarang   : programmer zaman now ",
+    "tahun terbit: 2019",
+};
+
+#endif
